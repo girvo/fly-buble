7e01d7d1628101ec5fe687d0058d6cb3b003c07b
Josh Girvin <josh@studionone.com.au>
2016-06-07 16:28:14 +1000

    Upgrading Bublé to v0.10.6
    Also adding Visual Studio Code editor configs, and adjusting test code formatting

--- test/test.js
+++ test/test.js
@@ -1,23 +1,24 @@
+/*eslint-env node, es6 */
 var test = require('tape').test
 var buble = require('../')
 
 test('fly-buble', function (t) {
-    t.plan(4)
+  t.plan(4)
 
-    buble.call({
-        filter: function (name, transform) {
-            const content = 'let a = 0'
-            const result = transform(content, {})
+  buble.call({
+    filter: function (name, transform) {
+      const content = 'let a = 0'
+      const result = transform(content, {})
 
-            t.equal(name, 'buble',
-                'add buble filter')
-            t.ok(/var a/.test(result.code),
-                'buble transform')
-            t.equal(result.ext, '.js',
-                'has the right extension')
-            t.equal(result.map.sourcesContent[0], content,
-                'source maps work')
-            t.end()
-        }
-    })
+      t.equal(name, 'buble',
+        'add buble filter')
+      t.ok(/var a/.test(result.code),
+        'buble transform')
+      t.equal(result.ext, '.js',
+        'has the right extension')
+      t.equal(result.map.sourcesContent[0], content,
+        'source maps work')
+      t.end()
+    }
+  })
 })
